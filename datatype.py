3c1780ba14df1dfad77cc664c148f8aad365426b
santiago <spereira98@misena.edu.co>
2021-08-19 10:05:06 -0500

    Tipos de datos
    Descripcuión y uso de los tipo de datos

---
+++ datatype.py
@@ -0,0 +1,36 @@
+
+#datos tipo string
+print("hola mundo")
+print('Hola Mundo')
+print('''Hola Mundo''')
+print("""Hola Mundo""")
+
+#datos tipo int
+print(19)
+
+#datos tipo float
+print(19.8)
+
+#datos tipo BOOLEAN
+True 
+False
+#se colocan los datos falso y verdadero con las primeras en mayuscula
+
+#datos tipo list
+[40, 60, 80, 20]
+['hola', 'buenas', 'buenos', 'dias', 'tardes', 'noches']
+['hola', True, 50.2, 'mundo']
+
+#datos tipo tuples
+(40, 60, 80, 20)
+
+#datos tipo dictorionies
+
+print(type({
+    "Nombre":"Emmanuel",
+    "Apellido":"Pereira",
+    "Apodo":"Boxeo",
+}))
+
+#tipo de dato Nonetype
+None
