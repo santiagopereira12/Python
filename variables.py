e3998dc8a41ea62d47de4e7a082de90d135c1f33
santiago <spereira98@misena.edu.co>
2021-08-19 10:00:00 -0500

    Variables
    Descripción y uso de variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+
+name = "Santiago"
+
+print(name)
+
+#conventions
+book_name = "Etica" #Snake case
+bookName = "Politica" #Camel case
+BookName = "Metafisica" #Pascal case
+
+#Constantes
+PI = 3.1416 #La variable constante se escribe en mayuscula para así dejar claro que es un valor que no cambia
+
+#variables dinamicas
+book_name = "Etica"
+book_name = 1001201789
