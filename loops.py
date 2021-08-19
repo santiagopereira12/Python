a04cdfe765d53eacaa5a3195e4e55180c187170f
santiago <spereira98@misena.edu.co>
2021-08-19 10:07:50 -0500

    Bucles
    Descripción del funcionamiento de los bucles

---
+++ loops.py
@@ -0,0 +1,19 @@
+
+foods = ['meat', 'milk', 'bread', 'potato', 'chicken', 'onion', 'cheese', 'money']
+
+for food in foods:
+    if food == "potato":
+        continue
+    print(food)
+
+for number in range(1, 16):
+    print(number)
+
+for letter in "Santiago":
+    print(letter)
+
+count = 4 
+
+while count <= 10:
+    print(count)
+    count = count + 1
