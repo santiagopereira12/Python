fecc62fd41ac3303446f590a9c836ae7947e60ec
santiago <spereira98@misena.edu.co>
2021-08-19 10:08:53 -0500

    Conjuntos
    Descripción y uso de los conjuntos

---
+++ sets.py
@@ -0,0 +1,13 @@
+
+colores = {'amarillo', 'azul', 'rojo'}
+print(type(colores))
+print('azul' in colores)
+print(colores)
+colores.add('morado')
+print(colores)
+colores.remove('amarillo')
+print(colores)
+colores.clear()
+print(colores)
+del colores
+print(colores)
