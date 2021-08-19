dc5ce9375cf203c96958ef4487ca46b13d36dded
santiago <spereira98@misena.edu.co>
2021-08-19 10:06:00 -0500

    Diccionarios
    Descripción y uso de los diccionarios

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+
+product = {
+    "name" : "shoes",
+    "quantity" : 2,
+    "price" : 57000
+}
+
+person = {
+    "First_name" : "Cristian",
+    "last_name" : "Prieto"
+}
+
+print(type(product))
+print(type(person))
+
+print(person.keys())
+print(person.items())
+
+products = {
+    {"name": 'phone', "price": 2500}, 
+    {"name": 'skate', "price": 1600}
+}
+
+print(products)
